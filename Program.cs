855f5d59830160a6c89ff18dbf0c46be60d49cea
ĹeticiaAraujo-dev <leticia.p.araujo04@gmail.com>
2020-11-13 16:42:52 -0300

    exercicio 3

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Terceiro_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //entrada de dados
+            Console.WriteLine("Digite o valor do seu salário:");
+            int salario = int.Parse(Console.ReadLine());
+
+            //processo
+            if(salario > 500){
+                Console.WriteLine("Você não receberá um aumento no salário");
+            }else{
+                int aumento = (salario * 30)/100;
+                int salarioN = aumento + salario;
+                Console.WriteLine("Você receberá um aumento de "+aumento+ " reais e seu salário novo será "+salarioN+ " reais");
+            }
+        }
+    }
+}
